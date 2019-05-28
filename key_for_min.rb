6a0ee33a5acf7fa904bf7ac4fa01404bdbbe3237
github username <github email address>
2019-05-27 22:47:44 -0400

    Using iteration to find a conditional value in a hash.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+    if name_hash === {}
+        return nil
+    else
+        lowest_value = name_hash.first[1]
+        lowest_key = name_hash.first[0]
+        name_hash.each do |key, value|
+            if value < lowest_value
+                lowest_value = value
+                lowest_key = key
+            end
+        end
+        lowest_key
+    end
 end
